fb846f1f7f58690d7ff1d93d421bd3f57b0d87e4
Patrik Olsson <paolsson@microsoft.com>
2019-05-22 09:07:21 +0100

    Elaborate comment

--- src/System/Validation/ValidatedNotNullAttribute.cs
+++ src/System/Validation/ValidatedNotNullAttribute.cs
@@ -6,8 +6,27 @@ using System;
 namespace Microsoft.Omex.System.Validation
 {
 	/// <summary>
-	/// Indicates to Code analysis tools that a method validates a particular parameter to not be null
+	/// Indicates to Code analysis tools that a method validates a particular parameter to not be null.
+	/// 
+	/// This can for instance be used to suppress VS design warning CA1062 (validate public method parameters before use) by applying to
+	/// external validation libraries, such as the `Code` class in this project.
+	/// 
+	/// If an attribute with the name ValidatedNotNull is present on the parameter of the validation method it tells the analyzer that the 
+	/// parameter will never be null in the code path following the execution of that method.
 	/// </summary>
+	/// <example>
+	/// In this example the Guard.NotNull has the attribute present on its parameter. If it wasn't present the analyzer would not know
+	/// that s cannot be null following the execution of the method, resulting in a warning on the execution of the `.toUpper` method.
+	/// <code>
+	/// string s = mightBeNullClass?.stringProperty;
+	///
+	/// Guard.NotNull(s);
+	///
+	/// // The analyzer now knows `s` will never be null here
+	/// return s.toUpper();
+	/// </code>
+	/// </example>
+	/// 
 	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
 	internal sealed class ValidatedNotNullAttribute : Attribute { }
 }
