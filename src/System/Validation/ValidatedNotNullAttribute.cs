fd2d96b6a68b4079fbf21cd2ef08bfdf108cbe05
Patrik Olsson <paolsson@microsoft.com>
2019-05-22 10:53:11 +0100

    Add see and c tags to documentation

--- src/System/Validation/ValidatedNotNullAttribute.cs
+++ src/System/Validation/ValidatedNotNullAttribute.cs
@@ -6,17 +6,18 @@ using System;
 namespace Microsoft.Omex.System.Validation
 {
 	/// <summary>
-	/// Indicates to Code analysis tools that a method validates a particular parameter to not be null.
+	/// Indicates to code analysis tools that a method validates a particular parameter to not be null.
 	/// 
 	/// This can for instance be used to suppress VS design warning CA1062 (validate public method parameters before use) by applying to
-	/// external validation libraries, such as the `Code` class in this project.
+	/// external validation libraries, such as the <see cref="Code" /> class in this project.
 	/// 
 	/// If an attribute with the name ValidatedNotNull is present on the parameter of the validation method it tells the analyzer that the 
 	/// parameter will never be null in the code path following the execution of that method.
 	/// </summary>
 	/// <example>
-	/// In this example the Guard.NotNull has the attribute present on its parameter. If it wasn't present the analyzer would not know
-	/// that s cannot be null following the execution of the method, resulting in a warning on the execution of the <c>toUpperInvariant</c> method.
+	/// In this example the <c>Guard.NotNull</c> has the attribute present on its parameter. If it wasn't present the analyzer would not know
+	/// that <c>s</c> cannot be <c>null</c> following the execution of the method, resulting in a warning on the execution of the
+	/// <c>toUpperInvariant</c> method.
 	/// <code>
 	/// string s = mightBeNullClass?.stringProperty;
 	///
