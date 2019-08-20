a983c1b617fbee6726f542134ad4ec9982a9caa7
Andrey Tretyak <andreyt@microsoft.com>
2019-08-20 12:24:40 +0100

    Add equalitu members for Category

--- src/System/Logging/Category.cs
+++ src/System/Logging/Category.cs
@@ -9,7 +9,7 @@ namespace Microsoft.Omex.System.Logging
 	/// <summary>
 	/// Logging category
 	/// </summary>
-	public class Category
+	public class Category : IEquatable<Category>
 	{
 		/// <summary>
 		/// Category
@@ -26,5 +26,54 @@ namespace Microsoft.Omex.System.Logging
 		/// Name
 		/// </summary>
 		public string Name { get; }
+
+
+		/// <summary>
+		/// Indicates whether the current object is equal to another object of the same type
+		/// </summary>
+		/// <param name="other">An object to compare with this object</param>
+		public bool Equals(Category other)
+		{
+			if (ReferenceEquals(null, other))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return string.Equals(Name, other.Name, StringComparison.InvariantCulture);
+		}
+
+
+		/// <summary>
+		/// Determines whether the specified <see cref="object" />, is equal to this instance
+		/// </summary>
+		/// <param name="obj">The <see cref="object" /> to compare with this instance</param>
+		public override bool Equals(object obj) => ReferenceEquals(this, obj) || obj is Category other && Equals(other);
+
+
+		/// <summary>
+		/// Returns a hash code for this instance.
+		/// </summary>
+		public override int GetHashCode() => Name != null ? StringComparer.InvariantCulture.GetHashCode(Name) : 0;
+
+
+		/// <summary>
+		/// Implements the operator ==
+		/// </summary>
+		/// <param name="left">The left</param>
+		/// <param name="right">The right</param>
+		public static bool operator ==(Category left, Category right) => Equals(left, right);
+
+
+		/// <summary>
+		/// Implements the operator !=
+		/// </summary>
+		/// <param name="left">The left</param>
+		/// <param name="right">The right</param>
+		public static bool operator !=(Category left, Category right) => !Equals(left, right);
 	}
 }
