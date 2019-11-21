c501ddf946cae097279982ec28e925f044de00be
Andrey Tretyak <andreyt@microsoft.com>
2019-11-21 17:32:34 +0000

    change factiry

--- src/System/TimedScopes/TimeScopeFactory.cs
+++ src/System/TimedScopes/TimeScopeFactory.cs
@@ -2,65 +2,37 @@
 // Licensed under the MIT license.
 
 using Microsoft.Omex.System.Diagnostics;
+using Microsoft.Omex.System.Logging;
 using Microsoft.Omex.System.TimedScopes.ReplayEventLogging;
+using Microsoft.Omex.System.Validation;
 
 namespace Microsoft.Omex.System.TimedScopes
 {
 	/// <summary>
 	/// Correlation Data
 	/// </summary>
-	public class TimeScopeFactory
+	public class TimedScopeFactory
 	{
-		/// <summary>
-		/// Correlation Data
-		/// </summary>
-		public CorrelationData CorrelationData { get; }
-
-
-		/// <summary>
-		/// Timed Scope Definition
-		/// </summary>
-		public IMachineInformation MachineInformation { get; }
-
-
-		/// <summary>
-		/// Timed Scope metrics logger
-		/// </summary>
-		private ITimedScopeLogger ScopeLogger { get; }
-
-
-		/// <summary>
-		/// Replay event configurator
-		/// </summary>
-		private IReplayEventConfigurator ReplayEventConfigurator { get; }
-
-
-		/// <summary>
-		/// Timed Scope Stack Manager
-		/// </summary>
-		public ITimedScopeStackManager TimedScopeStackManager { get; }
-
-
 		/// <summary>
 		/// Create a timed scope
 		/// </summary>
 		/// <param name="customLogger">Use a custom logger for the timed scope</param>
 		/// <param name="replayEventConfigurator">Replay event configurator</param>
 		/// <param name="timedScopeStackManager">Timed scope stack manager</param>
-		/// <param name="correlationData">Correlation data</param>
+		/// <param name="correlation">Correlation data</param>
 		/// <param name="machineInformation">Machine Information</param>
-		public TimeScopeFactory(
-			CorrelationData correlationData,
+		public TimedScopeFactory(
+			Correlation correlation,
 			IMachineInformation machineInformation,
 			ITimedScopeLogger customLogger,
 			IReplayEventConfigurator replayEventConfigurator,
 			ITimedScopeStackManager timedScopeStackManager)
 		{
-			CorrelationData = correlationData;
-			MachineInformation = machineInformation;
-			ScopeLogger = customLogger;
-			ReplayEventConfigurator = replayEventConfigurator;
-			TimedScopeStackManager = timedScopeStackManager;
+			m_correlation = Code.ExpectsArgument(correlation, nameof(correlation), TaggingUtilities.ReserveTag(0));
+			m_machineInformation = Code.ExpectsArgument(machineInformation, nameof(machineInformation), TaggingUtilities.ReserveTag(0));
+			m_scopeLogger = Code.ExpectsArgument(customLogger, nameof(customLogger), TaggingUtilities.ReserveTag(0));
+			m_replayEventConfigurator = Code.ExpectsArgument(replayEventConfigurator, nameof(replayEventConfigurator), TaggingUtilities.ReserveTag(0));
+			m_timedScopeStackManager = Code.ExpectsArgument(timedScopeStackManager, nameof(timedScopeStackManager), TaggingUtilities.ReserveTag(0));
 		}
 
 
@@ -72,7 +44,7 @@ namespace Microsoft.Omex.System.TimedScopes
 		/// <param name="startScope">Should the scope be automatically started (for use in e.g. 'using' statement)</param>
 		public TimedScope Create(TimedScopeDefinition definition, TimedScopeResult initialResult, bool startScope = true)
 		{
-			TimedScope scope = definition.Create(CorrelationData, MachineInformation, ScopeLogger, ReplayEventConfigurator, TimedScopeStackManager, initialResult, startScope);
+			TimedScope scope = definition.Create(m_correlation.CurrentCorrelation, m_machineInformation, m_scopeLogger, m_replayEventConfigurator, m_timedScopeStackManager, initialResult, startScope);
 			if (startScope == true)
 			{
 				scope.Start();
@@ -80,5 +52,12 @@ namespace Microsoft.Omex.System.TimedScopes
 
 			return scope;
 		}
+
+
+		private readonly Correlation m_correlation;
+		private readonly IMachineInformation m_machineInformation;
+		private readonly ITimedScopeLogger m_scopeLogger;
+		private readonly IReplayEventConfigurator m_replayEventConfigurator;
+		private readonly ITimedScopeStackManager m_timedScopeStackManager;
 	}
 }
