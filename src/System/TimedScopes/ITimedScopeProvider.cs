25399faee879e766faa897b8c1cbb8bc3159c628
Andrey Tretyak <andreyt@microsoft.com>
2019-12-04 20:18:37 +0000

    Add renamed file

---
+++ src/System/TimedScopes/ITimedScopeProvider.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Microsoft.Omex.System.TimedScopes
+{
+	public interface ITimedScopeProvider
+	{
+		TimedScope Create(TimedScopeDefinition definition, TimedScopeResult initialResult, bool startScope = true);
+	}
+}
