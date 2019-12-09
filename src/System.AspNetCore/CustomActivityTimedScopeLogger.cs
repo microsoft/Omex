736bbf566d5809e65007ee0ce2b22069ac27c747
Andrey Tretyak <andreyt@microsoft.com>
2019-12-09 09:06:23 +0000

    Add logger implementation

---
+++ src/System.AspNetCore/CustomActivityTimedScopeLogger.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Omex.System.Diagnostics;
+using Microsoft.Omex.System.Logging;
+using Microsoft.Omex.System.TimedScopes;
+using Microsoft.Omex.System.Validation;
+
+namespace Microsoft.Omex.System.AspNetCore
+{
+	/// <summary>
+	/// Logs timed scopes using a custom activity
+	/// </summary>
+	public class CustomActivityTimedScopeLogger : ITimedScopeLogger
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="eventSource">The event source for timed scopes</param>
+		/// <param name="machineInformation">Machine information</param>
+		public CustomActivityTimedScopeLogger(TimedScopeEventSource eventSource, IMachineInformation machineInformation)
+		{
+			m_eventSource = Code.ExpectsArgument(eventSource, nameof(eventSource), TaggingUtilities.ReserveTag(0));
+			m_machineInformation = Code.ExpectsArgument(machineInformation, nameof(machineInformation), TaggingUtilities.ReserveTag(0));
+		}
+
+
+		/// <summary>
+		/// Logs the scope start
+		/// </summary>
+		/// <param name="scope">Scope to log</param>
+		public void LogScopeStart(TimedScope scope)
+		{
+		}
+
+
+		/// <summary>
+		/// Logs the scope end
+		/// </summary>
+		/// <param name="scope">Scope to log</param>
+		/// <param name="data">Correlation data</param>
+		public void LogScopeEnd(TimedScope scope, CorrelationData data)
+		{
+			if (!Code.ValidateArgument(scope, nameof(scope), TaggingUtilities.ReserveTag(0)) ||
+				!Code.ValidateArgument(data, nameof(data), TaggingUtilities.ReserveTag(0)))
+			{
+				return;
+			}
+
+			if (scope.IsTransaction)
+			{
+				m_eventSource.LogEvent(Categories.TimingGeneral,
+					name: scope.Name,
+					subtype: scope.SubType ?? NullPlaceholder,
+					metadata: scope.MetaData ?? NullPlaceholder,
+					serviceName: ServiceName ?? NullPlaceholder,
+					result: scope.Result,
+					correlationId: data.VisibleId.ToString("D"),
+					durationMs: scope.DurationInMilliseconds);
+			}
+			else
+			{
+				m_eventSource.LogEvent(Categories.TimingGeneral,
+					name: scope.Name,
+					subtype: scope.SubType ?? NullPlaceholder,
+					metadata: scope.MetaData ?? NullPlaceholder,
+					serviceName: ServiceName ?? NullPlaceholder,
+					userHash: data.Data(TimedScopeDataKeys.InternalOnly.UserHash) ?? data.UserHash ?? NullPlaceholder,
+					result: scope.Result,
+					correlationId: data.VisibleId.ToString("D"),
+					durationMs: scope.DurationInMilliseconds);
+			}
+		}
+
+
+		/// <summary>
+		/// The service name
+		/// </summary>
+		private string ServiceName => m_machineInformation?.ServiceName?.ToString();
+
+
+		/// <summary>
+		/// Used as a dimension value for null scope dimensions
+		/// </summary>
+		private const string NullPlaceholder = "null";
+
+
+		/// <summary>
+		/// The event source for timed scopes
+		/// </summary>
+		private readonly TimedScopeEventSource m_eventSource;
+
+
+		/// <summary>
+		/// Machine information
+		/// </summary>
+		private readonly IMachineInformation m_machineInformation;
+	}
+}
---
+++ src/System.AspNetCore/TimedScopeEventSource.cs
@@ -0,0 +1,251 @@
+﻿/***************************************************************************************************
+	TimedScopeEventSource.cs
+
+	Copyright (c) Microsoft Corporation.
+
+	Timed scopes event source
+***************************************************************************************************/
+
+using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Omex.System.Logging;
+using Microsoft.Omex.System.TimedScopes;
+
+namespace Microsoft.Omex.System.AspNetCore
+{
+	/// <summary>
+	/// Timed scopes event source
+	/// </summary>
+	[EventSource(Name = "Microsoft-OMEX-TimedScopes")]
+	public sealed class TimedScopeEventSource : EventSource
+	{
+		#region Initialization
+
+		/// <summary>
+		/// An initialized instance of the <see cref="TimedScopeEventSource"/> class
+		/// The singleton behavior is needed so that the Event Source works properly
+		/// </summary>
+		public static TimedScopeEventSource Instance => s_instance.Value;
+
+
+		/// <summary>
+		/// Static constructor
+		/// </summary>
+		static TimedScopeEventSource()
+		{
+			// A workaround for the problem where ETW activities do not get tracked until Tasks infrastructure is initialized.
+			// This problem will be fixed in .NET Framework 4.6.2.
+			Task.Run(() => { });
+		}
+
+
+		/// <summary>
+		/// Private constructor to enforce singleton semantics needed for the Event Source to work properly
+		/// </summary>
+		private TimedScopeEventSource()
+			: base()
+		{
+		}
+
+
+		/// <summary>
+		/// Lazy instance of the <see cref="TimedScopeEventSource"/> class
+		/// </summary>
+		private static readonly Lazy<TimedScopeEventSource> s_instance = new Lazy<TimedScopeEventSource>(
+			() => new TimedScopeEventSource(),
+			LazyThreadSafetyMode.PublicationOnly);
+
+		#endregion
+
+
+		#region Non-Events
+
+		/// <summary>
+		/// Logs the occurrence of an activity
+		/// </summary>
+		/// <param name="logCategory">Log category</param>
+		/// <param name="name">TimedScope name</param>
+		/// <param name="subtype">TimedScope subtype</param>
+		/// <param name="metadata">TimedScope metadata</param>
+		/// <param name="userHash">User hash</param>
+		/// <param name="serviceName">Service name</param>
+		/// <param name="result">TimedScope result</param>
+		/// <param name="correlationId">Correlation Id</param>
+		/// <param name="durationMs">TimedScope duration in milliseconds</param>
+		[NonEvent]
+		public void LogEvent(
+				Category logCategory,
+				string name,
+				string subtype,
+				string metadata,
+				string userHash,
+				string serviceName,
+
+				TimedScopeResult result,
+				string correlationId,
+				double durationMs
+			)
+		{
+			string nameAsString = SanitizeMdmString(name, nameof(name), name, logCategory);
+			string subTypeAsString = SanitizeMdmString(subtype, nameof(subtype), name, logCategory);
+			string metaDataAsString = SanitizeMdmString(metadata, nameof(metadata), name, logCategory);
+			string userHashAsString = SanitizeMdmString(userHash, nameof(userHash), name, logCategory);
+			string serviceNameAsString = SanitizeMdmString(serviceName, nameof(serviceName), name, logCategory);
+			string correlationIdAsString = SanitizeMdmString(correlationId, nameof(correlationId), name, logCategory);
+
+			WriteTimedScopeEvent(
+				nameAsString,
+				subTypeAsString,
+				metaDataAsString,
+				userHashAsString,
+				serviceNameAsString,
+
+				logCategory.Name,
+				result.ToString(),
+				correlationIdAsString,
+				Convert.ToInt64(durationMs));
+		}
+
+
+
+		/// <summary>
+		/// Logs the occurrence of an activity
+		/// </summary>
+		/// <param name="logCategory">Log category</param>
+		/// <param name="name">TimedScope name</param>
+		/// <param name="subtype">TimedScope subtype</param>
+		/// <param name="metadata">TimedScope metadata</param>
+		/// <param name="serviceName">Service name</param>
+		/// <param name="result">TimedScope result</param>
+		/// <param name="correlationId">Correlation Id</param>
+		/// <param name="durationMs">TimedScope duration in milliseconds</param>
+		[NonEvent]
+		public void LogEvent(
+			Category logCategory,
+			string name,
+			string subtype,
+			string metadata,
+			string serviceName,
+
+			TimedScopeResult result,
+			string correlationId,
+			double durationMs
+		)
+		{
+			string nameAsString = SanitizeMdmString(name, nameof(name), name, logCategory);
+			string subTypeAsString = SanitizeMdmString(subtype, nameof(subtype), name, logCategory);
+			string metaDataAsString = SanitizeMdmString(metadata, nameof(metadata), name, logCategory);
+			string serviceNameAsString = SanitizeMdmString(serviceName, nameof(serviceName), name, logCategory);
+			string correlationIdAsString = SanitizeMdmString(correlationId, nameof(correlationId), name, logCategory);
+
+			WriteTimedScopeTrxEvent(
+				nameAsString,
+				subTypeAsString,
+				metaDataAsString,
+				serviceNameAsString,
+
+				logCategory.Name,
+				result.ToString(),
+				correlationIdAsString,
+				Convert.ToInt64(durationMs));
+		}
+
+
+		private static string SanitizeMdmString(string mdmString, string name, string activityName, Category logCategory)
+		{
+			string validatedString = Convert.ToString(mdmString) ?? string.Empty;
+
+			if (validatedString.Length > 1024)
+			{
+				ULSLogging.LogTraceTag(0x23857681 /* tag_97x0b */, logCategory, Levels.Warning, MdmStringLimitMessage, 1024, name, activityName, validatedString.Length);
+				validatedString = validatedString.Substring(0, 1024);
+			}
+
+			return validatedString;
+		}
+
+
+		private const string MdmStringLimitMessage =
+			"MDM enforces a string length limit of {0} characters per dimension. Truncating length of dimension {1} on activity {2} from {3} chars in order to allow upload of the metric";
+
+		#endregion
+
+
+		#region Events
+
+		/// <summary>
+		/// Writes a Timed scope event with Level=Informational.
+		/// </summary>
+		/// <param name="name">Timed scope name</param>
+		/// <param name="subType">Subtype</param>
+		/// <param name="metadata">Metadata</param>
+		/// <param name="serviceName">Service name</param>
+		/// <param name="userHash">User hash</param>
+		/// <param name="logCategory">Log category</param>
+		/// <param name="result">Result</param>
+		/// <param name="correlationId">Correlation Id</param>
+		/// <param name="durationMs">Duration in ms</param>
+		[Event((int)EventIds.LogTimedScopeEventId, Level = EventLevel.Informational, Version = 3)]
+		private void WriteTimedScopeEvent(
+			string name,
+			string subType,
+			string metadata,
+			string userHash,
+			string serviceName,
+			string logCategory,
+			string result,
+			string correlationId,
+			long durationMs)
+		{
+			WriteEvent((int)EventIds.LogTimedScopeEventId, name ?? string.Empty, subType ?? string.Empty, metadata ?? string.Empty, userHash ?? string.Empty, serviceName ?? string.Empty, logCategory, result, correlationId, durationMs);
+		}
+
+
+		/// <summary>
+		/// Writes a Timed scope trace event with Level=Informational.
+		/// </summary>
+		/// <param name="name">Timed scope name</param>
+		/// <param name="subType">Subtype</param>
+		/// <param name="metadata">Metadata</param>
+		/// <param name="serviceName">Service name</param>
+		/// <param name="logCategory">Log category</param>
+		/// <param name="result">Result</param>
+		/// <param name="correlationId">Correlation Id</param>
+		/// <param name="durationMs">Duration in ms</param>
+		[Event((int)EventIds.LogTimedScopeTrxEventId, Level = EventLevel.Informational, Version = 3)]
+		private void WriteTimedScopeTrxEvent(
+			string name,
+			string subType,
+			string metadata,
+			string serviceName,
+			string logCategory,
+			string result,
+			string correlationId,
+			long durationMs)
+		{
+			WriteEvent((int)EventIds.LogTimedScopeTrxEventId, name ?? string.Empty, subType ?? string.Empty, metadata ?? string.Empty, serviceName ?? string.Empty, logCategory, result, correlationId, durationMs);
+		}
+
+		#endregion
+
+
+		/// <summary>
+		/// Event ids enum
+		/// </summary>
+		public enum EventIds : ushort
+		{
+			/// <summary>
+			/// Event Id for logging timed scopes
+			/// </summary>
+			LogTimedScopeEventId = 6,
+
+
+			/// <summary>
+			/// Event Id for logging timed scopes traces
+			/// </summary>
+			LogTimedScopeTrxEventId = 7
+		}
+	}
+}
