c06e248b71c7f3fc875c98959045cb8d66aeb37b
Andrey Tretyak <andreyt@microsoft.com>
2020-02-24 07:02:25 +0000

    more timed spopes unit test partial commit

---
+++ tests/Extensions/TimedScopes.UnitTests/SimpleActivityProviderTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.TimedScopes.UnitTests
+{
+	[TestClass]
+	public class SimpleActivityProviderTests
+	{
+		[TestMethod]
+		public void CheckNameOfCreatedActivuty()
+		{
+			string testActivityName = "TestName";
+			Activity activity = new SimpleActivityProvider().Create(testActivityName);
+
+			Assert.IsNotNull(activity);
+
+			string actualName = activity.OperationName;
+
+			Assert.ReferenceEquals(testActivityName, actualName);
+		}
+	}
+
+
+	[TestClass]
+	public class TimedScopeTests
+	{
+		[TestMethod]
+		public void A()
+		{
+			TimedScopeProvider provider = new TimedScope();
+		}
+	}
+
+
+	[TestClass]
+	public class TimedScopeProviderTests
+	{
+		[TestMethod]
+		public void A()
+		{
+			TimedScopeProvider provider = new TimedScopeProvider();
+		}
+	}
+}
