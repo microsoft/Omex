fe255859f1c18e856b282da743fc07cbf9403705
Andrey Tretyak <andreyt@microsoft.com>
2020-02-25 11:28:25 -0800

    Partial commit host.service type registration unit tests + general InternalVisibleTo

--- tests/Extensions/Hosting.Services.UnitTests/HostBuilderExtensionsTests.cs
+++ tests/Extensions/Hosting.Services.UnitTests/HostBuilderExtensionsTests.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Omex.Extensions.Hosting.Services;
+using Microsoft.Omex.Extensions.Logging;
+using Microsoft.Omex.Extensions.TimedScopes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Hosting.Services.UnitTests
@@ -6,9 +11,24 @@ namespace Hosting.Services.UnitTests
 	[TestClass]
 	public class HostBuilderExtensionsTests
 	{
-		[TestMethod]
-		public void A()
+		[DataTestMethod]
+		[DataRow(typeof(IServiceContext), typeof(OmexServiceFabricContext))]
+		[DataRow(typeof(IMachineInformation), typeof(ServiceFabricMachineInformation))]
+		[DataRow(typeof(ITimedScopeProvider), null)]
+		[DataRow(typeof(ILogger), null)]
+		public void TestOmexTypeRegistration(Type typeToResolver, Type? expectedImplementationType)
 		{
+			object obj = new ServiceCollection()
+				.AddOmexServiceFabricDependencies()
+				.BuildServiceProvider()
+				.GetServices(typeToResolver);
+
+			Assert.IsNotNull(obj);
+
+			if (expectedImplementationType != null)
+			{
+				Assert.IsInstanceOfType(obj, expectedImplementationType);
+			}
 		}
 	}
 }
---
+++ tests/Extensions/Hosting.Services.UnitTests/ServiceFabricHostBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Fabric;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Omex.Extensions.Hosting.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hosting.Services.UnitTests
+{
+	[TestClass]
+	public class ServiceFabricHostBuilderTests
+	{
+		[TestMethod]
+		public void HostPropagatesTypesRegistration()
+		{
+			HostBuilder hostBuilder = new HostBuilder();
+
+			new ServiceFabricHostBuilder<ServiceContext>(hostBuilder)
+			.ConfigureServices((context, collection) =>
+			{
+				collection.AddTransient<TestTypeToResolve>();
+			});
+
+			TestTypeToResolve obj = hostBuilder.Build().Services.GetService<TestTypeToResolve>();
+
+			Assert.IsNotNull(obj);
+		}
+
+
+		private class TestTypeToResolve
+		{
+		}
+	}
+}
