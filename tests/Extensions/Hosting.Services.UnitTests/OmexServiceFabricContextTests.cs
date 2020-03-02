da5f8b18600a894db2e5cf66f81f4f5fc2d66e4c
Andrey Tretyak <andreyt@microsoft.com>
2020-03-02 22:40:36 +0000

    Add service accessor tests

--- tests/Extensions/Hosting.Services.UnitTests/OmexServiceFabricContextTests.cs
+++ tests/Extensions/Hosting.Services.UnitTests/OmexServiceFabricContextTests.cs
@@ -1,90 +1,41 @@
-﻿//// Copyright (c) Microsoft Corporation. All rights reserved.
-//// Licensed under the MIT license.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
-//using System.Fabric;
-//using Microsoft.Omex.Extensions.Hosting.Services;
-//using Microsoft.Omex.Extensions.Logging;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
+using System;
+using System.Fabric;
+using Microsoft.Omex.Extensions.Hosting.Services;
+using Microsoft.Omex.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
-//namespace Hosting.Services.UnitTests
-//{
-//	[TestClass]
-//	public class OmexServiceFabricContextTests
-//	{
-//		[TestMethod]
-//		public void TestStatelessWithValue()
-//		{
-//			Mock<IServiceContextAccessor<StatelessServiceContext>> mockContext = new Mock<IServiceContextAccessor<StatelessServiceContext>>();
-//			mockContext.SetupGet(c => c.ServiceContext).Returns(ServiceFabric.Mocks.MockStatelessServiceContextFactory.Default);
-//			IServiceContext context = new OmexServiceFabricContext(mockContext.Object);
-//			CheckCashedValue(context, mockContext);
-//		}
+namespace Hosting.Services.UnitTests
+{
+	[TestClass]
+	public class OmexServiceFabricContextTests
+	{
+		[TestMethod]
+		public void TestStateless() =>
+			TestContextSet(ServiceFabric.Mocks.MockStatelessServiceContextFactory.Default);
 
 
-//		[TestMethod]
-//		public void TestStatelessWithoutValue()
-//		{
-//			Mock<IServiceContextAccessor<StatelessServiceContext>> mockContext = new Mock<IServiceContextAccessor<StatelessServiceContext>>();
-//			mockContext.SetupGet(c => c.ServiceContext).Returns((StatelessServiceContext?)null);
-//			IServiceContext context = new OmexServiceFabricContext(mockContext.Object);
-//			CheckNotCashedValue(context, mockContext);
-//		}
+		[TestMethod]
+		public void TestStatefulWithValue() =>
+			TestContextSet(ServiceFabric.Mocks.MockStatefulServiceContextFactory.Default);
 
 
-//		[TestMethod]
-//		public void TestStatefulWithValue()
-//		{
-//			Mock<IServiceContextAccessor<StatefulServiceContext>> mockContext = new Mock<IServiceContextAccessor<StatefulServiceContext>>();
-//			mockContext.SetupGet(c => c.ServiceContext).Returns(ServiceFabric.Mocks.MockStatefulServiceContextFactory.Default);
-//			IServiceContext context = new OmexServiceFabricContext(mockContext.Object);
-//			CheckCashedValue(context, mockContext);
-//		}
+		public void TestContextSet<TContext>(TContext serviceContext)
+			where TContext : ServiceContext
+		{
+			ServiceContextAccessor<TContext> accessor = new ServiceContextAccessor<TContext>();
+			IServiceContext context = new OmexServiceFabricContext(accessor);
 
+			Assert.AreEqual(default, context.PartitionId);
+			Assert.AreEqual(default, context.ReplicaOrInstanceId);
 
-//		[TestMethod]
-//		public void TestStatefulWithoutValue()
-//		{
-//			Mock<IServiceContextAccessor<StatefulServiceContext>> mockContext = new Mock<IServiceContextAccessor<StatefulServiceContext>>();
-//			mockContext.SetupGet(c => c.ServiceContext).Returns((StatefulServiceContext?)null);
-//			IServiceContext context = new OmexServiceFabricContext(mockContext.Object);
-//			CheckNotCashedValue(context, mockContext);
-//		}
+			accessor.SetContext(serviceContext);
 
-
-//		private void CheckNotCashedValue<TContext>(IServiceContext context, Mock<IServiceContextAccessor<TContext>> mock)
-//			where TContext : ServiceContext
-//		{
-//			context.GetPartitionId();
-//			context.GetPartitionId();
-//			context.GetPartitionId();
-
-//			mock.VerifyGet(c => c.ServiceContext, Times.Exactly(3));
-
-//			context.GetReplicaOrInstanceId();
-//			context.GetReplicaOrInstanceId();
-//			context.GetReplicaOrInstanceId();
-//			context.GetReplicaOrInstanceId();
-
-//			mock.VerifyGet(c => c.ServiceContext, Times.Exactly(4));
-//		}
-
-
-//		private void CheckCashedValue<TContext>(IServiceContext context, Mock<IServiceContextAccessor<TContext>> mock)
-//			where TContext : ServiceContext
-//		{
-//			context.GetPartitionId();
-//			context.GetPartitionId();
-//			context.GetPartitionId();
-
-//			mock.VerifyGet(c => c.ServiceContext, Times.Once);
-
-//			context.GetReplicaOrInstanceId();
-//			context.GetReplicaOrInstanceId();
-//			context.GetReplicaOrInstanceId();
-//			context.GetReplicaOrInstanceId();
-
-//			mock.VerifyGet(c => c.ServiceContext, Times.Once);
-//		}
-//	}
-//}
+			Assert.AreEqual(serviceContext.PartitionId, context.PartitionId);
+			Assert.AreEqual(serviceContext.ReplicaOrInstanceId, context.ReplicaOrInstanceId);
+		}
+	}
+}
