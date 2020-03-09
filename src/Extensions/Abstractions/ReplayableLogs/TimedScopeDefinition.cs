10b24db30ba494468180b0517745a0066f3acaac
Andrey Tretyak <andreyt@microsoft.com>
2020-03-09 11:55:02 +0000

    Make TimedScope definition struct

--- src/Extensions/Abstractions/ReplayableLogs/TimedScopeDefinition.cs
+++ src/Extensions/Abstractions/ReplayableLogs/TimedScopeDefinition.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Omex.Extensions.Abstractions
 	/// <summary>
 	/// Store Timed Scope name
 	/// </summary>
-	public class TimedScopeDefinition
+	public readonly struct TimedScopeDefinition
 	{
 		/// <summary>
 		/// Timed Scope name
@@ -21,15 +21,45 @@ namespace Microsoft.Omex.Extensions.Abstractions
 		/// </summary>
 		/// <param name="name">Timed Scope name</param>
 		/// <exception cref="ArgumentException">Thrown when name null or empty</exception>
-		public TimedScopeDefinition(string name)
-		{
-			// Activity won't be stated in case of an empty string
-			if (string.IsNullOrWhiteSpace(name))
-			{
-				throw new ArgumentException("TimedScope name should not be empty or null");
-			}
+		public TimedScopeDefinition(string name) =>
+			Name = string.IsNullOrWhiteSpace(name)
+				? throw new ArgumentException("TimedScope name should not be empty or null") // Activity won't be stated in case of an empty string
+				: name;
 
-			Name = name;
-		}
+
+		/// <summary>
+		/// Indicates whether the current object is equal to another object of the same type
+		/// </summary>
+		/// <param name="other">An object to compare with this object</param>
+		public bool Equals(TimedScopeDefinition other) => string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+
+
+		/// <summary>
+		/// Determines whether the specified <see cref="object" />, is equal to this instance
+		/// </summary>
+		/// <param name="obj">The <see cref="object" /> to compare with this instance</param>
+		public override bool Equals(object? obj) => obj is TimedScopeDefinition other && Equals(other);
+
+
+		/// <summary>
+		/// Returns a hash code for this instance
+		/// </summary>
+		public override int GetHashCode() => StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
+
+
+		/// <summary>
+		/// Implements the operator ==
+		/// </summary>
+		/// <param name="left">The left</param>
+		/// <param name="right">The right</param>
+		public static bool operator ==(TimedScopeDefinition left, TimedScopeDefinition right) => Equals(left, right);
+
+
+		/// <summary>
+		/// Implements the operator !=
+		/// </summary>
+		/// <param name="left">The left</param>
+		/// <param name="right">The right</param>
+		public static bool operator !=(TimedScopeDefinition left, TimedScopeDefinition right) => !Equals(left, right);
 	}
 }
--- tests/Extensions/TimedScopes.UnitTests/TimedScopeDefinitionsTests.cs
+++ tests/Extensions/TimedScopes.UnitTests/TimedScopeDefinitionsTests.cs
@@ -29,5 +29,74 @@ namespace Microsoft.Omex.Extensions.TimedScopes.UnitTests
 			TimedScopeDefinition definition = new TimedScopeDefinition(activityName);
 			Assert.ReferenceEquals(activityName, definition.Name);
 		}
+
+
+		[TestMethod]
+		public void TestEquiality()
+		{
+			string name = nameof(TestEquiality);
+
+			TimedScopeDefinition definition1 = new TimedScopeDefinition(name);
+
+			TimedScopeDefinition definition2 = new TimedScopeDefinition(name);
+
+			Assert.IsTrue(definition1.Equals(definition2), "1 Equals 2");
+
+			Assert.IsTrue(definition2.Equals(definition1), "2 Equals 1");
+
+			Assert.IsTrue(definition1.Equals((object)definition2), "1 Equals (object)2");
+
+			Assert.IsTrue(definition2.Equals((object)definition1), "2 Equals (object)1");
+
+			Assert.IsTrue(definition1.GetHashCode() == definition2.GetHashCode(), "1 HashCode == 2 HashCode");
+
+			Assert.IsTrue(definition1 == definition2, "1 == 2");
+
+			Assert.IsTrue(definition2  == definition1, "2 == 1");
+
+			Assert.IsFalse(definition1 != definition2, "1 != 2");
+
+			Assert.IsFalse(definition2 != definition1, "2 != 1");
+		}
+
+
+		[TestMethod]
+		public void TestNonEquiality()
+		{
+			TimedScopeDefinition definition1 = new TimedScopeDefinition("TestName1");
+
+			TimedScopeDefinition definition2 = new TimedScopeDefinition("TestName2");
+
+			Assert.IsFalse(definition1.Equals(definition2), "1 Equals 2");
+
+			Assert.IsFalse(definition2.Equals(definition1), "2 Equals 1");
+
+			Assert.IsFalse(definition1.Equals((object)definition2), "1 Equals (object)2");
+
+			Assert.IsFalse(definition2.Equals((object)definition1), "2 Equals (object)1");
+
+			Assert.IsFalse(definition1.GetHashCode() == definition2.GetHashCode(), "1 HashCode == 2 HashCode");
+
+			Assert.IsFalse(definition1 == definition2, "1 == 2");
+
+			Assert.IsFalse(definition2 == definition1, "2 == 1");
+
+			Assert.IsTrue(definition1 != definition2, "1 != 2");
+
+			Assert.IsTrue(definition2 != definition1, "2 != 1");
+		}
+
+
+		[TestMethod]
+		public void TestEquialityWithEmpty()
+		{
+			TimedScopeDefinition definition = new TimedScopeDefinition(nameof(TestEquialityWithEmpty));
+
+			Assert.IsFalse(definition.Equals(null), "definition Equals null");
+
+			Assert.IsFalse(definition.Equals(default), "definition Equals default");
+
+			Assert.IsFalse(definition == default, "definition == null");
+		}
 	}
 }
