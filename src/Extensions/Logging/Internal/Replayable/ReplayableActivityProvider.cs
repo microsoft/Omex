8b8c77fb6e40a9f3e238d7c45df19413fa4f14f0
Andrey Tretyak <andreyt@microsoft.com>
2020-03-09 16:38:58 +0000

    Add comments

--- src/Extensions/Logging/Internal/Replayable/ReplayableActivityProvider.cs
+++ src/Extensions/Logging/Internal/Replayable/ReplayableActivityProvider.cs
@@ -8,6 +8,10 @@ using Microsoft.Omex.Extensions.TimedScopes;
 
 namespace Microsoft.Omex.Extensions.Logging.Replayable
 {
+	/// <summary>
+	/// Depending on <see cref="OmexLoggingOptions" /> class will create regular <see cref="Activity"/>
+	/// or <see cref="ReplayableActivity" /> that is able to aggregate log messages for replay
+	/// </summary>
 	internal class ReplayableActivityProvider : IActivityProvider
 	{
 		public ReplayableActivityProvider(IOptions<OmexLoggingOptions> options) =>
