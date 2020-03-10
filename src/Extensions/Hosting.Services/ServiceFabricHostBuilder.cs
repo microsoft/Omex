ced65d1d1f2c466931e274ec1303193ca0fcac45
Andrey Tretyak <andreyt@microsoft.com>
2020-03-10 16:25:57 +0000

    Rename generic parameters

--- src/Extensions/Hosting.Services/ServiceFabricHostBuilder.cs
+++ src/Extensions/Hosting.Services/ServiceFabricHostBuilder.cs
@@ -11,13 +11,13 @@ namespace Microsoft.Omex.Extensions.Hosting.Services
 	/// <summary>
 	/// Wrapper on top of IHostBuilder to propagete proper context type and avoid type registration mistakes
 	/// </summary>
-	public sealed class ServiceFabricHostBuilder<TContext> where TContext : ServiceContext
+	public sealed class ServiceFabricHostBuilder<TServiceContext> where TServiceContext : ServiceContext
 	{
 		/// <summary>
 		/// Method should be called only by extensions of this class
 		/// If you are creating a service please register dependencies before calling BuildService method and using this class
 		/// </summary>
-		internal ServiceFabricHostBuilder<TContext> ConfigureServices(Action<HostBuilderContext, IServiceCollection> action)
+		internal ServiceFabricHostBuilder<TServiceContext> ConfigureServices(Action<HostBuilderContext, IServiceCollection> action)
 		{
 			m_builder.ConfigureServices(action);
 			return this;
