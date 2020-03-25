17e6a0b0be86591f857139a512db582395738b89
Andrey Tretyak <andreyt@microsoft.com>
2020-03-25 15:12:33 +0000

    Add stub initializer for compatability classes to use in unit tests

---
+++ src/Extensions/Compatibility/TimedScopes/SimpleScopeProvider.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Omex.Extensions.Abstractions.Activities;
+
+namespace Microsoft.Omex.Extensions.Compatibility
+{
+	/// <summary>
+	/// Simple activity provider that could be used for unit tests
+	/// </summary>
+	/// <remarks>It does not support custom activity providers (log replay won't work)</remarks>
+	internal class SimpleScopeProvider : ITimedScopeProvider
+	{
+		public TimedScope Create(TimedScopeDefinition name, TimedScopeResult result = TimedScopeResult.SystemError) =>
+			new TimedScope(new System.Diagnostics.Activity(name.Name), result);
+
+		public TimedScope CreateAndStart(TimedScopeDefinition name, TimedScopeResult result = TimedScopeResult.SystemError) =>
+			Create(name, result).Start();
+	}
+}
