505236d54ab4f34076cfd28dbb98b1f0917eb2eb
Andrey Tretyak <andreyt@microsoft.com>
2020-03-26 11:03:09 +0000

    Make SimpleScopeProvider public for unittests

--- src/Extensions/Abstractions/Activities/SimpleScopeProvider.cs
+++ src/Extensions/Abstractions/Activities/SimpleScopeProvider.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-using Microsoft.Omex.Extensions.Abstractions.Activities;
-
-namespace Microsoft.Omex.Extensions.Compatibility
+namespace Microsoft.Omex.Extensions.Abstractions.Activities
 {
 	/// <summary>
 	/// Simple activity provider that could be used for unit tests
 	/// </summary>
 	/// <remarks>It does not support custom activity providers (log replay won't work)</remarks>
-	internal class SimpleScopeProvider : ITimedScopeProvider
+	public class SimpleScopeProvider : ITimedScopeProvider
 	{
+		/// <inheritdoc />
 		public TimedScope Create(TimedScopeDefinition name, TimedScopeResult result = TimedScopeResult.SystemError) =>
 			new TimedScope(new System.Diagnostics.Activity(name.Name), result);
 
+		/// <inheritdoc />
 		public TimedScope CreateAndStart(TimedScopeDefinition name, TimedScopeResult result = TimedScopeResult.SystemError) =>
 			Create(name, result).Start();
 	}
