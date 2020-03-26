06aef5496555556fb36e42e1777db90f510170e3
Andrey Tretyak <andreyt@microsoft.com>
2020-03-26 12:09:33 +0000

    Make SimpleScopeProvider public for unittests (#178)
    Make SimpleScopeProvider public for unit tests

---
+++ tests/Extensions/Abstractions.UnitTests/Activities/TimedScopeProviderTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Omex.Extensions.Abstractions.Activities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.TimedScopes.UnitTests
+{
+	[TestClass]
+	public class SimpleScopeProviderTests
+	{
+		[TestMethod]
+		public void CreateAndStart_TimedScopeCreated()
+		{
+			TimedScopeResult result = TimedScopeResult.ExpectedError;
+			TimedScopeDefinition definition = new TimedScopeDefinition(nameof(CreateAndStart_TimedScopeCreated));
+			SimpleScopeProvider provider = new SimpleScopeProvider();
+			TimedScope scope = provider.CreateAndStart(definition, result);
+			Assert.IsNotNull(scope);
+		}
+	}
+}
