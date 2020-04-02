1a0e45865723efe4e3de043a5c8da3de8d63fd1a
Andrey Tretyak <andreyt@microsoft.com>
2020-04-02 11:34:53 +0100

    Update src/Extensions/Hosting.Services.Web/KestrelListenerBuilder.cs
    Co-Authored-By: Vlad Ion <vlad.ion@gmail.com>

--- src/Extensions/Hosting.Services.Web/KestrelListenerBuilder.cs
+++ src/Extensions/Hosting.Services.Web/KestrelListenerBuilder.cs
@@ -78,7 +78,7 @@ namespace Microsoft.Omex.Extensions.Hosting.Services.Web
 				.AddSingleton<ActivityEnrichmentMiddleware>()
 				.AddSingleton<ResponseHeadersMiddleware>();
 
-#pragma warning disable CS0618 // We need to register all middlewares even obsolete
+#pragma warning disable CS0618 // We need to register all middlewares, even if obsolete
 			services.AddSingleton<ObsoleteCorrelationHeadersMiddleware>();
 #pragma warning restore CS0618
 		}
