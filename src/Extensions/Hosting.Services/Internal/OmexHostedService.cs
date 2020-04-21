e833e6118a643fcc2c7a43a84d2e4620febf55a9
Andrey Tretyak <andreyt@microsoft.com>
2020-04-21 23:07:36 +0100

    Swith from BackgroundService to IHostedService

--- src/Extensions/Hosting.Services/Internal/OmexHostedService.cs
+++ src/Extensions/Hosting.Services/Internal/OmexHostedService.cs
@@ -14,32 +14,76 @@ namespace Microsoft.Omex.Extensions.Hosting.Services
 	/// <summary>
 	/// Wraps service run into HostedService
 	/// </summary>
-	internal sealed class OmexHostedService : BackgroundService
+	internal sealed class OmexHostedService : IHostedService
 	{
+		private readonly IHostApplicationLifetime m_lifetime;
 		private readonly IOmexServiceRunner m_runner;
 		private readonly ILogger<OmexHostedService> m_logger;
+		private readonly CancellationTokenSource m_tokenSource;
 
-		public OmexHostedService(IOmexServiceRunner runner, ILogger<OmexHostedService> logger)
+		public OmexHostedService(IOmexServiceRunner runner, IHostApplicationLifetime lifetime, ILogger<OmexHostedService> logger)
 		{
 			m_runner = runner;
+			m_lifetime = lifetime;
 			m_logger = logger;
+			m_tokenSource = new CancellationTokenSource();
 		}
 
-		protected override async Task ExecuteAsync(CancellationToken token)
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			m_lifetime.ApplicationStarted.Register(OnServiceStarted);
+			m_lifetime.ApplicationStopping.Register(OnServiceStopping);
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+		private void OnServiceStarted() => RunServiceAsync().ContinueWith(OnRunnerCompleted);
+
+		private void OnServiceStopping()
+		{
+			if (m_tokenSource.IsCancellationRequested)
+			{
+				return;
+			}
+
+			m_tokenSource.Cancel();
+		}
+
+		private async Task RunServiceAsync()
 		{
 			try
 			{
-				ConfiguredTaskAwaitable task = m_runner.RunServiceAsync(token).ConfigureAwait(false);
+				await m_runner.RunServiceAsync(m_tokenSource.Token).ConfigureAwait(false);
 
 				m_logger.LogInformation(Tag.Create(), "ServiceFabricHost initialized");
-
-				await task;
 			}
 			catch (Exception e)
 			{
-				m_logger.LogCritical(Tag.Create(), e, "Exception during ServiceFabricHost execution");
+				m_logger.LogCritical(Tag.Create(), e, "Exception during ServiceFabricHost initialization");
+
+				m_lifetime.StopApplication();
+
 				throw;
 			}
+		}
+
+		private Task OnRunnerCompleted(Task task)
+		{
+			if (task.IsFaulted)
+			{
+				m_logger.LogCritical(Tag.Create(), task.Exception, "Service registration stoped due to exeption");
+			}
+			else if (task.IsCanceled)
+			{
+				m_logger.LogInformation(Tag.Create(), "Service registration canceled");
+			}
+			else
+			{
+				m_logger.LogInformation(Tag.Create(), "Service registration stopped");
+			}
+
+			return task;
 		}
 	}
 }
