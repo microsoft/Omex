d7adeed5f865c75d4112fb16183cf8b917bb66a2
Andrey Tretyak <andreyt@microsoft.com>
2020-06-11 21:19:55 +0100

    Add validation helpers

---
+++ src/Extensions/Abstractions/Validation.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Omex.Extensions.Abstractions
+{
+	/// <summary>
+	/// Class provides common validation methods
+	/// </summary>
+	public static class Validation
+	{
+		/// <summary>
+		/// Throw exception if value is null, otherwise returns not-nullable value
+		/// </summary>
+		/// <typeparam name="T">type of the value</typeparam>
+		/// <param name="value">value to check</param>
+		/// <param name="name">name of the value for exception message</param>
+		/// <exception cref="ArgumentNullException">if value is null</exception>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T ThrowIfNull<T>(T? value, string? name = null) where T : class
+			=> value ?? throw new ArgumentNullException(name);
+
+		/// <summary>
+		/// Throw exception if value is null, otherwise returns not-nullable value
+		/// </summary>
+		/// <typeparam name="T">type of the value</typeparam>
+		/// <param name="value">value to check</param>
+		/// <param name="name">name of the value for exception message</param>
+		/// <exception cref="ArgumentNullException">if value is null</exception>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T ThrowIfNull<T>(T? value, string? name = null) where T : struct
+			=> value ?? throw new ArgumentNullException(name);
+
+		/// <summary>
+		/// Throw exception if string is null or empty or white space, otherwise returns not-nullable value
+		/// </summary>
+		/// <param name="value">value to check</param>
+		/// <param name="name">name of the value for exception message</param>
+		/// <exception cref="ArgumentNullException">if value is null</exception>
+		/// <exception cref="ArgumentException">if value is empty or white space</exception>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static string ThrowIfNullOrWhiteSpace(string? value, string? name = null)
+		{
+			if (!string.IsNullOrWhiteSpace(value))
+			{
+				return value!; // `!` required because in net472 IsNullOrWhiteSpace does not have proper attributes
+			}
+
+			if (ThrowIfNull(value, name).Length == 0)
+			{
+				throw new ArgumentException("String is empty", name);
+			}
+
+			throw new ArgumentException("String contains only white space characters", name);
+		}
+	}
+}
---
+++ tests/Extensions/Abstractions.UnitTests/ValidationTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.Abstractions.UnitTests
+{
+	[TestClass]
+	public class ValidationTests
+	{
+		[TestMethod]
+		public void ThrowIfNullOrWhiteSpace_WhenValueValidRefType_ReturnsIt()
+		{
+			object expected = new object();
+			object actual = Validation.ThrowIfNull(expected);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void ThrowIfNullOrWhiteSpace_WhenValueValidStruct_ReturnsIt()
+		{
+			int? expected = 1;
+			int actual = Validation.ThrowIfNull(expected);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void ThrowIfNull_WhenValueNull_ThrowsException()
+			=> Assert.ThrowsException<ArgumentNullException>(() => Validation.ThrowIfNull((object?)null));
+
+		[TestMethod]
+		public void ThrowIfNull_WhenValueNullableStruct_ThrowsException()
+			=> Assert.ThrowsException<ArgumentNullException>(() => Validation.ThrowIfNull((int?)null));
+
+		[DataTestMethod]
+		[DataRow("asdsa")]
+		[DataRow("  a   ")]
+		[DataRow("-")]
+		public void ThrowIfNullOrWhiteSpace_WhenValueValid_ReturnsIt(string expected)
+		{
+			string actual = Validation.ThrowIfNullOrWhiteSpace(expected);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void ThrowIfNullOrWhiteSpace_WhenValueNull_ThrowsException() =>
+			Assert.ThrowsException<ArgumentNullException>(() => Validation.ThrowIfNullOrWhiteSpace(null));
+
+		[DataTestMethod]
+		[DataRow("")]
+		[DataRow("     ")]
+		[DataRow("\t\r\n")]
+		public void ThrowIfNullOrWhiteSpace_WhenValueEmptyOrWhiteSpace_ThrowsException(string value) =>
+			Assert.ThrowsException<ArgumentException>(() => Validation.ThrowIfNullOrWhiteSpace(value));
+	}
+}
