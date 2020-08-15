394d76de2b096d896d4a363b84a5f837615dfe78
Andrey Tretyak <andreyt@microsoft.com>
2020-08-15 23:09:38 +0100

    Bug fix, pass report data if healthcheck failed with Exception

---
+++ tests/Extensions/Diagnostics.HealthChecks.UnitTests/HealthCheckParametersTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.Diagnostics.HealthChecks.UnitTests
+{
+	[TestClass]
+	public class HealthCheckParametersTests
+	{
+		[DataTestMethod]
+		[DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+		public void Constructor_SetsReportData(KeyValuePair<string, object>[] reportData)
+		{
+			HealthCheckParameters parameters = new HealthCheckParameters(reportData);
+			CollectionAssert.AreEquivalent(reportData, parameters.ReportData.ToArray(), nameof(HttpHealthCheckParameters.ReportData));
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[] { Array.Empty<KeyValuePair<string, object>>() };
+			yield return new object[]
+			{
+				new Dictionary<string, object>
+				{
+					{ "key2", "SomeValue" },
+					{ "key1", new object() }
+				}.ToArray()
+			};
+		}
+	}
+}
