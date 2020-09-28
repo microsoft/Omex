a8b731c5d38e47da50e0c4425c05ed8099cc2eb5
Andrey Tretyak <andreyt@microsoft.com>
2020-09-28 15:32:14 +0100

    Report RootId as obsolete correlation if it's not added

--- src/Extensions/Logging/Internal/EventSource/OmexLogEventSender.cs
+++ src/Extensions/Logging/Internal/EventSource/OmexLogEventSender.cs
@@ -64,7 +64,7 @@ namespace Microsoft.Omex.Extensions.Logging
 				if (m_options.Value.AddObsoleteCorrelationToActivity)
 				{
 #pragma warning disable CS0618 // We are using obsolete correlation to support logging correlation from old Omex services
-					obsoleteCorrelationId = activity.GetObsoleteCorrelationId() ?? Guid.Empty;
+					obsoleteCorrelationId = activity.GetObsoleteCorrelationId() ?? activity.GetRootIdAsGuid() ?? Guid.Empty;
 					obsoleteTransactionId = activity.GetObsolteteTransactionId() ?? 0u;
 #pragma warning restore CS0618
 				}
