1202b6f4626477e1d1ee9205107713ceea923396
Enjak <miroljub.enjakovic@supper.london>
2020-10-16 17:11:59 +0200

    Added copyright headers to new files

--- tests/Extensions/Hosting.Services.Remoting.UnitTests/MockServiceWithDependencies.cs
+++ tests/Extensions/Hosting.Services.Remoting.UnitTests/MockServiceWithDependencies.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Omex.Extensions.Hosting.Services.Remoting.UnitTests
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Microsoft.Omex.Extensions.Hosting.Services.Remoting.UnitTests
 {
 	internal class MockServiceWithDependencies : IMockService
 	{
