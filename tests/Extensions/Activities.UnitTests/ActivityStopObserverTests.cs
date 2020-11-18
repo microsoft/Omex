80c443cbb30edbaacd20164ac7247bd8d04b29e6
Andrey Tretyak <andreyt@microsoft.com>
2020-11-18 23:05:09 +0000

    Partial commit

---
+++ tests/Extensions/Activities.UnitTests/ActivityStopObserverTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Hosting.Services.UnitTests
+{
+	[TestClass]
+	public class ActivityStopObserverTests
+	{
+		[TestMethod]
+		public void OnStop_CallsLogActivityStop()
+		{
+			Activity activity = new Activity(nameof(OnStop_CallsLogActivityStop));
+			Mock<ActivitySource> senderMock = new ();
+			ActivityStopObserver observer = new ActivityStopObserver(senderMock.Object);
+			observer.OnStop(activity, null);
+			senderMock.Verify(s => s.LogActivityStop(activity), Times.Once);
+		}
+	}
+}
---
+++ tests/Extensions/Activities.UnitTests/SimpleActivityProviderTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Diagnostics;
+using Microsoft.Omex.Extensions.Abstractions.Activities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.TimedScopes.UnitTests
+{
+	[TestClass]
+	public class SimpleActivityProviderTests
+	{
+		[DataTestMethod]
+		[DataRow("TestName")]
+		[DataRow("123")]
+		public void Create_CreatesActivity(string expectedName)
+		{
+			Activity activity = new SimpleActivityProvider().Create(new TimedScopeDefinition(expectedName));
+
+			Assert.IsNotNull(activity);
+
+			string actualName = activity.OperationName;
+
+			Assert.AreEqual(expectedName, actualName);
+		}
+	}
+}
