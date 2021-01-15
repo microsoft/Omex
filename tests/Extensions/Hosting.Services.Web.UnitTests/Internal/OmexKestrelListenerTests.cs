45bf0f0c0ee0cfdf4481a1aa0d72d8ada3404b17
Andrey Tretyak <andreyt@microsoft.com>
2021-01-15 22:17:51 +0000

    Unit tests

--- tests/Extensions/Hosting.Services.Web.UnitTests/Internal/OmexKestrelListenerTests.cs
+++ tests/Extensions/Hosting.Services.Web.UnitTests/Internal/OmexKestrelListenerTests.cs
@@ -1,20 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Hosting.Services.Web.UnitTests;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -22,39 +13,11 @@ using Moq;
 namespace Microsoft.Omex.Extensions.Hosting.Services.Web.UnitTests.Internal
 {
 	[TestClass]
-	public class ApplicationBuilderExtensionsTests
-	{
-		[TestMethod]
-		public void BuildStatelessWebService_UseUniqueServiceUrl_Failing()
-		{
-			Assert.ThrowsException<ArgumentException>(() =>
-			{
-				new HostBuilder().BuildStatelessWebService<MockStartup>(
-					"someService1",
-					new WebEndpointInfo[0],
-					ServiceFabricIntegrationOptions.UseUniqueServiceUrl | ServiceFabricIntegrationOptions.UseReverseProxyIntegration);
-			});
-		}
-
-		[TestMethod]
-		public void BuildStatelessWebService_UseUniqueServiceUrl_Failing()
-		{
-			IHost host = new HostBuilder().BuildStatelessWebService<MockStartup>(
-				"someService2",
-				WebEndpointInfo.CreateHttp());
-
-
-
-			IListenerBuilder<OmexStatelessService>[] builders = host.Services.GetRequiredService<IEnumerable<IListenerBuilder<OmexStatelessService>>>().ToArray();
-		}
-	}
-
-	[TestClass]
 	public class OmexKestrelListenerTests
 	{
 		[DataTestMethod]
-		[DataRow("somehost1", 483, new[] { "https://[::]:20081", "https://[::]:80", "https://[::]:483" }, "https://somehost:483")]
-		[DataRow("somehost2", 8082, new[] { "https://+:8081", "https://+:8082", "https://+:8083" }, "https://somehost:8082")]
+		[DataRow("somehost1", 483, new[] { "https://[::]:20081", "https://[::]:80", "https://[::]:483" }, "https://somehost1:483")]
+		[DataRow("somehost2", 8082, new[] { "https://+:8081", "https://+:8082", "https://+:8083" }, "https://somehost2:8082")]
 		public async Task OpenAsync_ReturnsProperAddress(string host, int port, string[] addresses, string expected)
 		{
 			string actual = await CreateListener(host, port, addresses).OpenAsync(CancellationToken.None);
@@ -82,54 +45,5 @@ namespace Microsoft.Omex.Extensions.Hosting.Services.Web.UnitTests.Internal
 
 			return new OmexKestrelListener(serverMock.Object, publishAddress, port);
 		}
-	}
-
-	[TestClass]
-	public class WebEndpointInfoTests
-	{
-		[TestMethod]
-		public void CreateHttp_SetsProperties()
-		{
-			string endpointName = "TestHttpEndpointName";
-			int port = 8081;
-
-			SetPortVariable(endpointName, port);
-
-			WebEndpointInfo info = WebEndpointInfo.CreateHttp(endpointName);
-			Assert.AreEqual(endpointName, info.Name);
-			Assert.IsNull(info.SettingForCertificateCommonName);
-			Assert.IsFalse(info.UseHttps);
-			Assert.AreEqual(port, info.Port);
-			Assert.AreEqual($"http://+:{port}", info.GetListenerUrl());
-		}
-
-		[TestMethod]
-		public void CreateHttps_SetsProperties()
-		{
-			string endpointName = "TestHttpsEndpointName";
-			string settingForCertificateCommonName = "SomeCertName";
-			int port = 8083;
-
-			SetPortVariable(endpointName, port);
-
-			WebEndpointInfo info = WebEndpointInfo.CreateHttps(endpointName, settingForCertificateCommonName);
-			Assert.AreEqual(endpointName, info.Name);
-			Assert.AreEqual(settingForCertificateCommonName, info.SettingForCertificateCommonName);
-			Assert.IsTrue(info.UseHttps);
-			Assert.AreEqual(port, info.Port);
-			Assert.AreEqual($"https://+:{port}", info.GetListenerUrl());
-		}
-
-		[TestMethod]
-		public void CreateHttp_WithEmptyEndpointName_Throws() => Assert.ThrowsException<ArgumentException>(() => WebEndpointInfo.CreateHttp(string.Empty));
-
-		[TestMethod]
-		public void CreateHttps_WithEmptyEndpointName_Throws() => Assert.ThrowsException<ArgumentException>(() => WebEndpointInfo.CreateHttps(string.Empty));
-
-		[TestMethod]
-		public void CreateHttp_WithWrongEndpointName_Throws() => Assert.ThrowsException<ArgumentException>(() => WebEndpointInfo.CreateHttp("UnexistingEndpoint"));
-
-		private void SetPortVariable(string name, int port) =>
-			Environment.SetEnvironmentVariable(SfConfigurationProvider.EndpointPortEvnVariableSuffix + name, port.ToString(), EnvironmentVariableTarget.Process);
 	}
 }
---
+++ tests/Extensions/Hosting.Services.Web.UnitTests/WebEndpointInfoTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.Hosting.Services.Web.UnitTests.Internal
+{
+	[TestClass]
+	public class WebEndpointInfoTests
+	{
+		[TestMethod]
+		public void CreateHttp_SetsProperties()
+		{
+			string endpointName = "TestHttpEndpointName";
+			int port = 8081;
+
+			SfConfigurationProviderHelper.SetPortVariable(endpointName, port);
+
+			WebEndpointInfo info = WebEndpointInfo.CreateHttp(endpointName);
+			Assert.AreEqual(endpointName, info.Name);
+			Assert.IsNull(info.SettingForCertificateCommonName);
+			Assert.IsFalse(info.UseHttps);
+			Assert.AreEqual(port, info.Port);
+			Assert.AreEqual($"http://+:{port}", info.GetListenerUrl());
+		}
+
+		[TestMethod]
+		public void CreateHttps_SetsProperties()
+		{
+			string endpointName = "TestHttpsEndpointName";
+			string settingForCertificateCommonName = "SomeCertName";
+			int port = 8083;
+
+			SfConfigurationProviderHelper.SetPortVariable(endpointName, port);
+
+			WebEndpointInfo info = WebEndpointInfo.CreateHttps(endpointName, settingForCertificateCommonName);
+			Assert.AreEqual(endpointName, info.Name);
+			Assert.AreEqual(settingForCertificateCommonName, info.SettingForCertificateCommonName);
+			Assert.IsTrue(info.UseHttps);
+			Assert.AreEqual(port, info.Port);
+			Assert.AreEqual($"https://+:{port}", info.GetListenerUrl());
+		}
+
+		[TestMethod]
+		public void CreateHttp_WithEmptyEndpointName_Throws() => Assert.ThrowsException<ArgumentException>(() => WebEndpointInfo.CreateHttp(string.Empty));
+
+		[TestMethod]
+		public void CreateHttps_WithEmptyEndpointName_Throws() => Assert.ThrowsException<ArgumentException>(() => WebEndpointInfo.CreateHttps(string.Empty));
+
+		[TestMethod]
+		public void CreateHttp_WithWrongEndpointName_Throws() => Assert.ThrowsException<ArgumentException>(() => WebEndpointInfo.CreateHttp("UnexistingEndpoint"));
+	}
+}
