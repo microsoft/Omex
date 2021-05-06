ca335c6af1e63cb07ee0080c6b6435624ee77e83
mvxxx <michal.tyrolski@gmail.com>
2021-05-06 16:25:47 +0100

    Service Fabric Client Factory

---
+++ src/Extensions/ServiceFabricGuest.Abstractions/factory/ServiceFabricClientFactory.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Omex.Extensions.ServiceFabricGuest.Abstractions
+{
+	/// <summary>
+	/// Factory for service fabric client wrapper
+	/// </summary>
+	public class ServiceFabricClientFactory
+	{
+		/// <summary>
+		/// Creates service fabric client wrapper instance and returns it
+		/// </summary>
+		/// <param name="options">REST client options</param>
+		/// <returns></returns>
+		public static IServiceFabricClientWrapper CreateServiceFabricClientWrapper(IOptions<ServiceFabricRestClientOptions> options)
+		{
+			return new ServiceFabricClientWrapper(options);
+		}
+	}
+}
