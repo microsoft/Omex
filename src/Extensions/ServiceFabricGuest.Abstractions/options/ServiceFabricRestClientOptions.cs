b2af1c10eb04345d6d9cb57dec3c476f08966e58
mvxxx <michal.tyrolski@gmail.com>
2021-05-24 16:56:32 +0100

    cluster endpoints default

--- src/Extensions/ServiceFabricGuest.Abstractions/options/ServiceFabricRestClientOptions.cs
+++ src/Extensions/ServiceFabricGuest.Abstractions/options/ServiceFabricRestClientOptions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Microsoft.Omex.Extensions.ServiceFabricGuest.Abstractions
 {
@@ -11,9 +12,39 @@ namespace Microsoft.Omex.Extensions.ServiceFabricGuest.Abstractions
 	public class ServiceFabricRestClientOptions
 	{
 		/// <summary>
-		/// Settings definition for Cluster Endpoint
+		/// Settings definition for Cluster Endpoint Port
+		/// </summary>
+		public int ClusterEndpointPort { get; set; } = 19080;
+
+		/// <summary>
+		/// Settings definition for Cluster Endpoint FQDN
 		/// </summary>
 		[Required(AllowEmptyStrings = false)]
-		public string ClusterEndpoint { get; set; } = string.Empty;
+		public string ClusterEndpointFQDN { get; set; } = DefaultFQDN();
+
+
+		/// <summary>
+		/// Settings definition for Cluster Endpoint 
+		/// </summary>
+		public string ClusterEndpoint => string.Format("http://{0}:{1}", ClusterEndpointFQDN, ClusterEndpointPort.ToString());
+
+		internal static string DefaultFQDN()
+		{
+			string? runtimeAddress = SfConfigurationProvider.GetSfVariable(RuntimeConnectionAddressEvnVariableName);
+			if (runtimeAddress == null)
+			{
+				return string.Empty;
+			}
+
+			string[] parts = runtimeAddress.Split(':');
+			if(parts.Length < 2)
+			{
+				return string.Empty;
+			}
+
+			return string.Join(":", parts.Take(parts.Length - 1));
+		}
+
+		internal const string RuntimeConnectionAddressEvnVariableName = "Fabric_RuntimeConnectionAddress";
 	}
 }
---
+++ tests/Extensions/ServiceFabricGuest.Abstractions.UnitTests/ServiceFabricRestClientOptionsTests.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Omex.Extensions.ServiceFabricGuest.Abstractions.UnitTests
+{
+	[TestClass]
+	public class ServiceFabricRestClientOptionsTests
+	{
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_Default_FQDN_Invalid()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "localhost");
+			ServiceFabricRestClientOptions options = new();
+
+			// Act.
+			string clusterEndpoint = options.ClusterEndpointFQDN;
+
+			// Assert.
+			Assert.AreEqual(string.Empty, clusterEndpoint);
+		}
+
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_Default_FQDN_Valid_Single()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "localhost:19081");
+			ServiceFabricRestClientOptions options = new();
+
+			// Act.
+			string clusterEndpoint = options.ClusterEndpointFQDN;
+
+			// Assert.
+			Assert.AreEqual("localhost", clusterEndpoint);
+		}
+
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_Default_FQDN_Valid_Multi()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "local:host:19081");
+			ServiceFabricRestClientOptions options = new();
+
+			// Act.
+			string clusterEndpoint = options.ClusterEndpointFQDN;
+
+			// Assert.
+			Assert.AreEqual("local:host", clusterEndpoint);
+		}
+
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_Default_Port()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "localhost:19081");
+			const int defaultPort = 19080;
+			ServiceFabricRestClientOptions options = new()
+			{
+				ClusterEndpointPort = defaultPort
+			};
+
+			// Act.
+			int port = options.ClusterEndpointPort;
+
+			// Assert.
+			Assert.AreEqual(port, defaultPort);
+		}
+
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_Override_FQDN()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "localhost:19081");
+			const string overrideFQDN = "myFQDN";
+			ServiceFabricRestClientOptions options = new()
+			{
+				ClusterEndpointFQDN = overrideFQDN
+			};
+
+			// Act.
+			string fqdn = options.ClusterEndpointFQDN;
+
+			// Assert.
+			Assert.AreEqual(fqdn, overrideFQDN);
+		}
+
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_EndPoint_Build()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "localhost:19081");
+			const string overrideFQDN = "myFQDN";
+			const int defaultPort = 19080;
+
+			ServiceFabricRestClientOptions options = new()
+			{
+				ClusterEndpointFQDN = overrideFQDN,
+				ClusterEndpointPort = defaultPort
+			};
+
+			// Act.
+			string endpoint = "http://myFQDN:19080";
+
+			// Assert.
+			Assert.AreEqual(endpoint, options.ClusterEndpoint);
+		}
+
+		[TestMethod]
+		public void ServiceFabricRestClientOptions_EndPoint_Default_Build()
+		{
+			// Arrange.
+			Environment.SetEnvironmentVariable(ServiceFabricRestClientOptions.RuntimeConnectionAddressEvnVariableName, "localhost:19081");
+
+			ServiceFabricRestClientOptions options = new();
+
+			// Act.
+			string endpoint = "http://localhost:19080";
+
+			// Assert.
+			Assert.AreEqual(endpoint, options.ClusterEndpoint);
+		}
+	}
+}
