5fbb9d236434d04055b39a8d599018ab5c891a22
Andrey Tretyak <andreyt@microsoft.com>
2021-09-22 01:31:17 +0100

    small fixes

--- src/Extensions/Abstractions/Activities/ActivityExtensions.cs
+++ src/Extensions/Abstractions/Activities/ActivityExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using Microsoft.Omex.Extensions.Abstractions.Activities.Processing;
