c433053a7253484e3a1895dacee08c796a417daa
Muiris Woulfe <mwoulfe@microsoft.com>
2021-10-13 17:23:25 +0100

    Tidying interface

--- src/Extensions/Activities/Internal/EventSource/IActivityEventSender.cs
+++ src/Extensions/Activities/Internal/EventSource/IActivityEventSender.cs
@@ -1,15 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Microsoft.Omex.Extensions.Activities.Internal.EventSource
+namespace Microsoft.Omex.Extensions.Activities
 {
-	interface IActivityEventSender
+	/// <summary>
+	/// An internal interface to allow dependency injection to select between <see cref="ActivityEventSender"/> and
+	/// <see cref="ScrubbedActivityEventSender"/>.
+	/// </summary>
+	internal interface IActivityEventSender : IActivitiesEventSender
 	{
 	}
 }
