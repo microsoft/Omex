79b4c8618d0a957e98cfa73293aeac08a9ed9b71
Keith Cully <kecully@microsoft.com>
2022-08-31 15:26:57 +0100

    Add conditional logic base on target framework

--- src/Logging/InitializationLogger.cs
+++ src/Logging/InitializationLogger.cs
@@ -41,7 +41,12 @@ namespace Microsoft.Omex.Extensions.Logging
 		public static void LogInitializationSucceed(string serviceNameForLogging, string message = "")
 		{
 			string logMessage = $"Initialization successful for {serviceNameForLogging}, {message}";
+#if Net_Standard
+			using Process process = Process.GetCurrentProcess();
+			ServiceInitializationEventSource.Instance.LogHostBuildSucceeded(process.Id, serviceNameForLogging, logMessage);
+#else
 			ServiceInitializationEventSource.Instance.LogHostBuildSucceeded(Environment.ProcessId, serviceNameForLogging, logMessage);
+#endif
 			Instance.LogInformation(Tag.Create(), logMessage);
 		}
 
