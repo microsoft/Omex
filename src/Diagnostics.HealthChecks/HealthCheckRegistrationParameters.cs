4e8410becf43d64101c2cb780b151b87e3dda1be
Francesco Bonacci <fbonacci@microsoft.com>
2022-08-31 23:02:27 +0100

    Introduce name, IsEnabled in HealthCheckRegistrationParameters

--- src/Diagnostics.HealthChecks/HealthCheckRegistrationParameters.cs
+++ src/Diagnostics.HealthChecks/HealthCheckRegistrationParameters.cs
@@ -15,26 +15,35 @@ public class HealthCheckRegistrationParameters
 	/// <summary>
 	/// Creates a new <see cref="T:Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckRegistrationParameters" />.
 	/// </summary>
+	/// <param name="name">A <see cref="string"/> indicating the name of the check.</param>
+	/// <param name="isEnabled">An optional <see cref="bool"/> indicating whether the check should be run.</param>
 	/// <param name="delay">An optional <see cref="TimeSpan"/> representing the initial delay applied after the application starts before executing the check.</param>
 	/// <param name="period">An optional <see cref="TimeSpan"/> representing the individual period of the check.</param>
 	/// <param name="timeout">An optional <see cref="TimeSpan"/> representing the individual timeout of the check.</param>
-	public HealthCheckRegistrationParameters(TimeSpan? delay = default, TimeSpan? period = default, TimeSpan? timeout = default)
+	public HealthCheckRegistrationParameters(string name, bool isEnabled = true, TimeSpan? delay = default, TimeSpan? period = default, TimeSpan? timeout = default)
 	{
+		Name = name;
+		IsEnabled = isEnabled;
 		Delay = delay;
 		Period = period;
 		Timeout = timeout;
 	}
 
 	/// <summary>
+	/// Gets the name of the health check.
+	/// </summary>
+	public string Name { get; }
+
+	/// <summary>
 	/// Gets the initial individual delay applied to the
-	/// individual HealthCheck after the application starts before executing.
+	/// individual health check after the application starts before executing.
 	/// The delay is applied once at startup, and does
 	/// not apply to subsequent iterations.
 	/// </summary>
 	public TimeSpan? Delay { get; }
 
 	/// <summary>
-	/// Gets the individual period used for the check.
+	/// Gets the individual period used for the health check.
 	/// </summary>
 	public TimeSpan? Period { get; }
 
@@ -43,4 +52,9 @@ public class HealthCheckRegistrationParameters
 	/// Use <see cref="System.Threading.Timeout.InfiniteTimeSpan"/> to execute with no timeout.
 	/// </summary>
 	public TimeSpan? Timeout { get; }
+
+	/// <summary>
+	/// Gets or sets whether the health check should be run. Enabled by default.
+	/// </summary>
+	public bool IsEnabled { get; set; } = true;
 }
