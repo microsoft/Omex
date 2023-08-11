72ad9b21a122543d9750daa7fa3f1b41fed33f08
Massimiliano D'Acunzo <mdacunzo@microsoft.com>
2023-08-11 13:52:51 +0100

    First commit

---
+++ src/Diagnostics.HealthChecks/Composables/HealthCheckComposablesExtensions.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Omex.Extensions.Diagnostics.HealthChecks.Composables;
+
+/// <summary>
+/// Extension methods for health check decorators configuration.
+/// </summary>
+public static class HealthCheckComposablesExtensions
+{
+	/// <summary>
+	/// Makes the Health check executable only at startup time, adding also observability to it.
+	/// If the consumer health check of this extension method does not want automatic observability,
+	/// the developer is encouraged to wrap it directly around the <see cref="StartupHealthCheck"/> class.
+	/// </summary>
+	/// <param name="healthCheck">The health check.</param>
+	/// <param name="cache">The memory cache. It will be used to memorise the latest health check result.</param>
+	/// <param name="activitySource">The activity source.</param>
+	/// <param name="logger">The logger.</param>
+	/// <param name="parameters">The health check parameters, set at DI registration time.</param>
+	/// <returns>The health check executable only at deploy time.</returns>
+	public static IHealthCheck AsObservableStartupHealthCheck(
+		this IHealthCheck healthCheck,
+		IMemoryCache cache,
+		ActivitySource activitySource,
+		ILogger logger,
+		HealthCheckParameters? parameters = null)
+	{
+		IHealthCheck startupHealthCheck = new StartupHealthCheck(healthCheck, cache);
+		return new ObservableHealthCheck(parameters ?? new(), startupHealthCheck, activitySource, logger);
+	}
+
+	/// <summary>
+	/// Applies the default error handling and activity around the current health check.
+	/// </summary>
+	/// The instance of the Activity marker. By selecting the implementor, the health check will determine how
+	/// the related endpoint will process the health check request.
+	/// <param name="healthCheck">The health check.</param>
+	/// <param name="activitySource">The activity source.</param>
+	/// <param name="logger">The logger.</param>
+	/// <param name="parameters">The health check parameters, set at DI registration time.</param>
+	/// <returns>The health check with the default configuration and error handling.</returns>
+	public static IHealthCheck AsObservableHealthCheck(
+		this IHealthCheck healthCheck,
+		ActivitySource activitySource,
+		ILogger logger,
+		HealthCheckParameters? parameters = null) =>
+		new ObservableHealthCheck(parameters ?? new(), healthCheck, activitySource, logger);
+
+	/// <summary>
+	/// Creates a new HTTP endpoint checker health check.
+	/// </summary>
+	/// <param name="httpClientFactory">The http client factory.</param>
+	/// <param name="requestBuilder">The request builder.</param>
+	/// <param name="healthCheckResponseChecker">The response checker instance.</param>
+	/// <param name="activitySource">The activity source.</param>
+	/// <param name="httpClientName">The HTTP Client name, if a client with that name was registered in particular.</param>
+	/// <param name="activityMarker">Marks the activity with the required baggage items.</param>
+	/// <returns>The HTTP endpoint checker health check.</returns>
+	public static IHealthCheck CreateHttpHealthCheck(
+		Func<HttpRequestMessage> requestBuilder,
+		Func<HealthCheckContext, HttpResponseMessage, CancellationToken, Task<HealthCheckResult>> healthCheckResponseChecker,
+		ActivitySource activitySource,
+		IHttpClientFactory httpClientFactory,
+		string? httpClientName = "",
+		Action<Activity?>? activityMarker = null) =>
+			new HttpEndpointHealthCheck(
+				httpClientFactory,
+				requestBuilder,
+				healthCheckResponseChecker,
+				activitySource,
+				httpClientName: httpClientName,
+				activityMarker: activityMarker);
+
+	/// <summary>
+	/// Checks whether the response from the endpoint is successful or not by checking its HTTP Status Code.
+	/// </summary>
+	/// <param name="context">The Health Check Context.</param>
+	/// <param name="response">The endpoint response.</param>
+	/// <param name="allowedStatusCodes">The allowed status codes for the response..</param>
+	/// <returns>The health check result.</returns>
+	public static Task<HealthCheckResult> CheckResponseStatusCodeAsync(
+		HealthCheckContext context,
+		HttpResponseMessage response,
+		HttpStatusCode[]? allowedStatusCodes = null)
+	{
+		if (allowedStatusCodes == null || allowedStatusCodes.Length == 0)
+		{
+			allowedStatusCodes = new[] { HttpStatusCode.OK };
+		}
+
+		if (allowedStatusCodes.Contains(response.StatusCode))
+		{
+			return Task.FromResult(new HealthCheckResult(HealthStatus.Healthy, "The endpoint returned an allowed status code."));
+		}
+
+		return Task.FromResult(
+			new HealthCheckResult(
+				context.Registration.FailureStatus,
+				$"The endpoint returned an unallowed status code. Status code returned: '{response.StatusCode}'. " +
+				$"Allowed status codes: '{string.Join(", ", allowedStatusCodes.Select(h => h.ToString()))}'"));
+	}
+}
---
+++ tests/Diagnostics.HealthChecks.UnitTests/Composables/ObservableHealthCheckTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Omex.Extensions.Diagnostics.HealthChecks.Composables;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Microsoft.Omex.Extensions.Diagnostics.HealthChecks.UnitTests.Composables;
+
+[TestClass]
+public class ObservableHealthCheckTests
+{
+	private static readonly ActivitySource s_activitySource = new(nameof(ObservableHealthCheckTests));
+
+	private static readonly ILogger s_logger = NullLogger.Instance;
+
+	[TestMethod]
+	[DataRow(HealthStatus.Healthy, HealthStatus.Degraded, HealthStatus.Healthy)]
+	[DataRow(HealthStatus.Healthy, HealthStatus.Unhealthy, HealthStatus.Healthy)]
+	[DataRow(HealthStatus.Unhealthy, HealthStatus.Degraded, HealthStatus.Degraded)]
+	[DataRow(HealthStatus.Unhealthy, HealthStatus.Unhealthy, HealthStatus.Unhealthy)]
+	[DataRow(HealthStatus.Degraded, HealthStatus.Degraded, HealthStatus.Degraded)]
+	[DataRow(HealthStatus.Degraded, HealthStatus.Unhealthy, HealthStatus.Unhealthy)]
+	public async Task MonitorHealthCheck_ShouldReturnGivenState_WhenHealthCheckHasGivenStatus(
+		HealthStatus healthStatus,
+		HealthStatus registeredFailureStatus,
+		HealthStatus expectedStatus)
+	{
+		Mock<IHealthCheck> healthCheckMock = new();
+		healthCheckMock.Setup(hc => hc.CheckHealthAsync(It.IsAny<HealthCheckContext>(), It.IsAny<CancellationToken>()))
+			.ReturnsAsync(new HealthCheckResult(healthStatus));
+
+		ObservableHealthCheck monitorHealthCheck = new(new(), healthCheckMock.Object, s_activitySource, s_logger);
+
+		CancellationTokenSource source = new();
+
+		HealthCheckResult result = await monitorHealthCheck.CheckHealthAsync(
+			HealthCheckTestHelpers.GetHealthCheckContext(monitorHealthCheck, registeredFailureStatus),
+			source.Token);
+
+		Assert.IsNotNull(result);
+		Assert.AreEqual(expectedStatus, result.Status);
+	}
+
+	[TestMethod]
+	public async Task MonitorHealthCheck_ShouldReturnUnhealthy_WhenHealthCheckThrowsException()
+	{
+		HealthCheckTestHelpers.InMemoryLogger logger = HealthCheckTestHelpers.GetLoggerInstance();
+
+		const string ExceptionMessage = "Exception message";
+		Mock<IHealthCheck> healthCheckMock = new();
+		healthCheckMock.Setup(hc => hc.CheckHealthAsync(It.IsAny<HealthCheckContext>(), It.IsAny<CancellationToken>()))
+			.ThrowsAsync(new InvalidOperationException(ExceptionMessage));
+
+		ObservableHealthCheck monitorHealthCheck = new(new(), healthCheckMock.Object, s_activitySource, logger);
+
+		CancellationTokenSource source = new();
+		HealthCheckContext context = HealthCheckTestHelpers.GetHealthCheckContext(monitorHealthCheck);
+
+		HealthCheckResult result = await monitorHealthCheck.CheckHealthAsync(
+			context,
+			source.Token);
+
+		Assert.IsNotNull(result);
+		Assert.AreEqual(context.Registration.FailureStatus, result.Status);
+	}
+}
