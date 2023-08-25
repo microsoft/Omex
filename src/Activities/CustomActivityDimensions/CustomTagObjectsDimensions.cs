5e9f19a65578e253d2a3200c9a8bca7700e76d98
Gnol Long <zzsushiboyzz@gmail.com>
2023-08-25 14:17:49 +0100

    Rename "Extra" to "Custom" for dimension set (#603)
    * Update
    
    * Rename to Custom

---
+++ src/Activities/CustomActivityDimensions/CustomTagObjectsDimensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using Microsoft.Omex.Extensions.Abstractions.Activities.Processing;
+
+namespace Microsoft.Omex.Extensions.Activities
+{
+	/// <inheritdoc/>
+	public class CustomTagObjectsDimensions : ICustomTagObjectsDimensions
+	{
+		/// <summary>
+		/// Default constructor which has <seealso cref="ActivityTagKeys.Result"/>, <seealso cref="ActivityTagKeys.SubType"/>, <seealso cref="ActivityTagKeys.Metadata"/>
+		/// </summary>
+		public CustomTagObjectsDimensions() => CustomDimensions = new HashSet<string>() { ActivityTagKeys.Result, ActivityTagKeys.SubType, ActivityTagKeys.Metadata };
+
+		/// <summary>
+		/// Override the default constructor to specify custom dimension set.
+		/// </summary>
+		/// <param name="overrideDefaultCustomDimensions">Set of custom dimensions which are added to metric point</param>
+		public CustomTagObjectsDimensions(HashSet<string> overrideDefaultCustomDimensions) => CustomDimensions = overrideDefaultCustomDimensions;
+
+		/// <inheritdoc/>
+		public HashSet<string> CustomDimensions { get; }
+	}
+}
---
+++ src/Activities/CustomActivityDimensions/ICustomBaggageDimensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Omex.Extensions.Activities
+{
+	/// <summary>
+	/// Define custom metric dimensions which are extracted from Activity Baggage  by <see cref="ActivityMetricsSender.SendActivityMetric"/>
+	/// Make sure that the value of any custom dimensions are not random values such as ID, timestamp because it will lead to metric explosion.
+	/// </summary>
+	public interface ICustomBaggageDimensions
+	{
+		/// <summary>
+		/// Custom dimension set
+		/// </summary>
+		HashSet<string> CustomDimensions { get; }
+	}
+}
