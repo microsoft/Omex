978b83a3b4fe2041a612f280e1b1d38fe7af959a
Artem Pushkin <arpushki@microsoft.com>
2023-10-19 22:28:07 +0100

    Removed options from OmexLoggerProvider.

--- src/Logging/Internal/OmexLoggerProvider.cs
+++ src/Logging/Internal/OmexLoggerProvider.cs
@@ -3,46 +3,35 @@
 
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Options;
 using Microsoft.Omex.Extensions.Logging.Replayable;
 using Microsoft.Omex.Extensions.Logging.Scrubbing;
 
 namespace Microsoft.Omex.Extensions.Logging
 {
+	[ProviderAlias("Omex")]
 	internal class OmexLoggerProvider : ILoggerProvider, ISupportExternalScope
 	{
 		public OmexLoggerProvider(
-			IOptions<OmexLoggingOptions> options,
 			ILogEventSender logsEventSender,
 			IExternalScopeProvider defaultExternalScopeProvider,
 			IEnumerable<ILogScrubbingRule> textScrubbers,
 			ILogEventReplayer? replayer = null)
 		{
-			m_options = options;
 			m_logsEventSender = logsEventSender;
 			m_defaultExternalScopeProvider = defaultExternalScopeProvider;
 			m_textScrubbers = textScrubbers;
 			m_replayer = replayer;
 		}
 
-		public ILogger CreateLogger(string categoryName)
-		{
-			if (m_options.Value.Enabled)
-			{
-				return new OmexLogger(m_logsEventSender, m_externalScopeProvider ?? m_defaultExternalScopeProvider, m_textScrubbers, categoryName, m_replayer);
-			}
+		public ILogger CreateLogger(string categoryName) =>
+			return new OmexLogger(m_logsEventSender, m_externalScopeProvider ?? m_defaultExternalScopeProvider, m_textScrubbers, categoryName, m_replayer);
 
-			return NullLogger.Instance;
-		}
-		
 		public void Dispose() { }
 
 		public void SetScopeProvider(IExternalScopeProvider scopeProvider) => m_externalScopeProvider = scopeProvider;
 
 		private IExternalScopeProvider? m_externalScopeProvider;
 
-		private readonly IOptions<OmexLoggingOptions> m_options;
 		private readonly ILogEventSender m_logsEventSender;
 		private readonly IExternalScopeProvider m_defaultExternalScopeProvider;
 		private readonly IEnumerable<ILogScrubbingRule> m_textScrubbers;
