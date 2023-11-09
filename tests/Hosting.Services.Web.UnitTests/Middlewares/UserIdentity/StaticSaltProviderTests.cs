a9ef28d3174047c98361959b15406187acfc868c
xiaoyushi <shixiaoyu0606@126.com>
2023-11-09 10:14:27 +0000

    update test

--- tests/Hosting.Services.Web.UnitTests/Middlewares/UserIdentity/StaticSaltProviderTests.cs
+++ tests/Hosting.Services.Web.UnitTests/Middlewares/UserIdentity/StaticSaltProviderTests.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-using System;
-using System.Linq;
-using System.Text;
+using Microsoft.Extensions.Options;
 using Microsoft.Omex.Extensions.Hosting.Services.Web.Middlewares;
+using Microsoft.Omex.Extensions.Hosting.Services.Web.Middlewares.UserIdentity.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Omex.Extensions.Hosting.Services.Web.UnitTests
@@ -15,12 +14,19 @@ namespace Microsoft.Omex.Extensions.Hosting.Services.Web.UnitTests
 		[TestMethod]
 		public void GetSalt_ReturnsConsistentResult()
 		{
-            string saltString1 = "testSaltString1";
-            string saltString2 = "testSaltString2";
+			IOptions<StaticSaltProviderOptions> options1 = Options.Create(new StaticSaltProviderOptions
+			{
+				SaltValue = "testSaltString1"
+			});
 
-			ISaltProvider provider1 = new StaticSaltProvider(saltString1);
-            ISaltProvider provider2 = new StaticSaltProvider(saltString1);
-            ISaltProvider provider3 = new StaticSaltProvider(saltString2);
+			IOptions<StaticSaltProviderOptions> options2 = Options.Create(new StaticSaltProviderOptions
+			{
+				SaltValue = "testSaltString2"
+			});
+
+			ISaltProvider provider1 = new StaticSaltProvider(options1);
+            ISaltProvider provider2 = new StaticSaltProvider(options1);
+            ISaltProvider provider3 = new StaticSaltProvider(options2);
 
 			CollectionAssert.AreEqual(provider1.GetSalt().ToArray(), provider2.GetSalt().ToArray());
             CollectionAssert.AreNotEqual(provider1.GetSalt().ToArray(), provider3.GetSalt().ToArray());
