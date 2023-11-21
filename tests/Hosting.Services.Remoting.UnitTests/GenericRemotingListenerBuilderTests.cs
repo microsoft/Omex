d7dca5b431cb611c28766b999df85fcfc5735838
Keith Cully <kecully@microsoft.com>
2023-11-21 13:15:37 +0000

    Update UTs

--- tests/Hosting.Services.Remoting.UnitTests/GenericRemotingListenerBuilderTests.cs
+++ tests/Hosting.Services.Remoting.UnitTests/GenericRemotingListenerBuilderTests.cs
@@ -4,6 +4,7 @@
 using System;
 using Microsoft.Omex.Extensions.Hosting.Services.UnitTests;
 using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport.Runtime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -13,7 +14,7 @@ namespace Microsoft.Omex.Extensions.Hosting.Services.Remoting.UnitTests
 	public class GenericRemotingListenerBuilderTests
 	{
 		[TestMethod]
-		public void GenericRemotingListenerBuilder_PropagatesParameters()
+		public void GenericRemotingListenerBuilder_NoTransportSettigns_ThrowsException()
 		{
 			string name = "TestName";
 			MockService mockService = new();
@@ -28,6 +29,30 @@ namespace Microsoft.Omex.Extensions.Hosting.Services.Remoting.UnitTests
 						Assert.AreEqual(mockProvider, p);
 						return mockService;
 					});
+
+			Assert.AreEqual(name, builder.Name);
+
+			Assert.ThrowsException<InvalidOperationException>(() => builder.BuildService(omexStatefulService));
+		}
+		
+		[TestMethod]
+		public void GenericRemotingListenerBuilder_WithTransportSettigns_PropagatesParameters()
+		{
+			string name = "TestName";
+			MockService mockService = new();
+			IServiceProvider mockProvider = new Mock<IServiceProvider>().Object;
+			OmexStatefulService omexStatefulService = MockServiceFabricServices.MockOmexStatefulService;
+			FabricTransportRemotingListenerSettings transportSettings = new();
+
+			GenericRemotingListenerBuilder<OmexStatefulService> builder =
+				new(name, mockProvider,
+					(p, s) =>
+					{
+						Assert.AreEqual(omexStatefulService, s);
+						Assert.AreEqual(mockProvider, p);
+						return mockService;
+					},
+					transportSettings);
 
 			Assert.AreEqual(name, builder.Name);
 
