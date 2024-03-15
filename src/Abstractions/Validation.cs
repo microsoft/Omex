de828d8f6ba1ca3c9b76741354f291b1fcebccfb
Neil <49037171+neilr81@users.noreply.github.com>
2024-03-15 08:40:37 -0700

    Adding NetStandard for Abstractions project (#664)
    * Adding NetStandard support for Abstractions Library
    
    Netstandard is still needed in one place down the chain. Updated all other projects to use netcore versions and reduced library targets to the Abstractions project
    
    * remove netcore check
    
    ---------
    
    Co-authored-by: Neil Richardson <neil.richardson@microsoft.com>

--- src/Abstractions/Validation.cs
+++ src/Abstractions/Validation.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Omex.Extensions.Abstractions
 		{
 			if (!string.IsNullOrWhiteSpace(value))
 			{
-				return value;
+				return value!; // `!` required because in netstandard2.0 IsNullOrWhiteSpace does not have proper attributes
 			}
 
 			_ = value ?? throw new ArgumentNullException(name);
