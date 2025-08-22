97c07dc8f0badf45c5f08ff345a23076e567cc44
Muiris Woulfe <Muiris.Woulfe@microsoft.com>
2025-08-22 17:16:55 +0100

    Reverting unnecessary changes

--- src/Diagnostics.HealthChecks/HealthCheckConstants.cs
+++ src/Diagnostics.HealthChecks/HealthCheckConstants.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
 
 namespace Microsoft.Omex.Extensions.Diagnostics.HealthChecks;
 
--- tests/Diagnostics.HealthChecks.AspNetCore.UnitTests/StartupHealthCheckTests.cs
+++ tests/Diagnostics.HealthChecks.AspNetCore.UnitTests/StartupHealthCheckTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
 
 using System.Diagnostics;
 using System.Threading;
